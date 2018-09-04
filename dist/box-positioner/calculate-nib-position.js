5b6454490e5fade9aeb64ed22f1e6f7457f9fdab
Endri Gjiri <egjiri@gmail.com>
2018-09-04 16:14:56 -0400

    Fix bug with calculateNibPosition where the nib ends up outside the element

--- dist/box-positioner/calculate-nib-position.js
+++ dist/box-positioner/calculate-nib-position.js
@@ -3,44 +3,60 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const calculate_position_1 = require("./calculate-position");
 function calculateNibPosition(options, nibWidth, nibHeight) {
     const position = calculate_position_1.default(options);
-    const nibHalfWidth = nibWidth / 2;
     const nibHalfHeight = nibHeight / 2;
     const elementWidth = options.element.width;
     const elementHeight = options.element.height;
-    const elementHalfWidth = elementWidth / 2 - nibHalfWidth;
     const elementHalfHeight = elementHeight / 2 - nibHalfHeight;
     const { point, direction } = position;
-    let leftOffset = options.target.x - (point.x + elementHalfWidth + nibHalfWidth);
-    if (direction.pointsLeft) {
-        leftOffset += elementWidth;
-    }
+    const left = calculateLeft(direction, options.target.x, point.x, elementWidth, nibWidth);
     if (direction.pointsDown) {
-        return {
-            top: -nibHeight,
-            left: leftOffset + elementHalfWidth,
-            transform: 'rotate(0deg)'
-        };
+        return buildNibPosition(direction, -nibHeight, left);
     }
     else if (direction.pointsLeft) {
-        return {
-            top: elementHalfHeight,
-            left: elementWidth - 2,
-            transform: 'rotate(90deg)'
-        };
+        return buildNibPosition(direction, elementHalfHeight, elementWidth - 2);
     }
     else if (direction.pointsUp) {
-        return {
-            top: elementHeight,
-            left: leftOffset + elementHalfWidth,
-            transform: 'rotate(180deg)'
-        };
+        return buildNibPosition(direction, elementHeight, left);
     }
     else {
-        return {
-            top: elementHalfHeight,
-            left: -nibWidth + 2,
-            transform: 'rotate(270deg)'
-        };
+        return buildNibPosition(direction, elementHalfHeight, -nibWidth + 2);
     }
 }
 exports.default = calculateNibPosition;
+function calculateLeft(direction, targetX, pointX, elementWidth, nibWidth) {
+    const nibHalfWidth = nibWidth / 2;
+    const elementHalfWidth = elementWidth / 2 - nibHalfWidth;
+    let leftOffset = targetX - (pointX + elementHalfWidth + nibHalfWidth);
+    if (direction.pointsLeft) {
+        leftOffset += elementWidth;
+    }
+    const left = leftOffset + elementHalfWidth;
+    if (left < 0 || left + nibWidth > elementWidth) {
+        return null;
+    }
+    return left;
+}
+function buildNibPosition(direction, top, left) {
+    let transform;
+    if (direction.pointsDown) {
+        transform = 'rotate(0deg)';
+    }
+    else if (direction.pointsLeft) {
+        transform = 'rotate(90deg)';
+    }
+    else if (direction.pointsUp) {
+        transform = 'rotate(180deg)';
+    }
+    else {
+        transform = 'rotate(270deg)';
+    }
+    if (left === null) {
+        top = 0;
+        left = 0;
+    }
+    return {
+        top: top,
+        left: left,
+        transform: transform
+    };
+}
--- src/box-positioner/calculate-nib-position.ts
+++ src/box-positioner/calculate-nib-position.ts
@@ -1,42 +1,58 @@
 import calculatePosition from './calculate-position';
 import { calculationOptions, nibPosition } from './types';
+import Direction from './direction';
 
 export default function calculateNibPosition(options: calculationOptions, nibWidth: number, nibHeight: number): nibPosition {
   const position = calculatePosition(options);
-  const nibHalfWidth = nibWidth / 2;
   const nibHalfHeight = nibHeight / 2;
   const elementWidth = options.element.width;
   const elementHeight = options.element.height;
-  const elementHalfWidth = elementWidth / 2 - nibHalfWidth;
   const elementHalfHeight = elementHeight / 2 - nibHalfHeight;
   const { point, direction } = position;
-  let leftOffset = options.target.x - (point.x + elementHalfWidth + nibHalfWidth);
+  const left = calculateLeft(direction, options.target.x, point.x, elementWidth, nibWidth);
+  if (direction.pointsDown) {
+    return buildNibPosition(direction, -nibHeight, left);
+  } else if (direction.pointsLeft) {
+    return buildNibPosition(direction, elementHalfHeight, elementWidth - 2);
+  } else if (direction.pointsUp) {
+    return buildNibPosition(direction, elementHeight, left);
+  } else {
+    return buildNibPosition(direction, elementHalfHeight, -nibWidth + 2);
+  }
+}
+
+function calculateLeft(direction: Direction, targetX: number, pointX: number, elementWidth: number, nibWidth: number): number | null {
+  const nibHalfWidth = nibWidth / 2;
+  const elementHalfWidth = elementWidth / 2 - nibHalfWidth;
+  let leftOffset = targetX - (pointX + elementHalfWidth + nibHalfWidth);
   if (direction.pointsLeft) {
     leftOffset += elementWidth;
   }
+  const left = leftOffset + elementHalfWidth;
+  if (left < 0 || left + nibWidth > elementWidth) {
+    return null
+  }
+  return left;
+}
+
+function buildNibPosition(direction: Direction, top: number, left: number | null): nibPosition {
+  let transform
   if (direction.pointsDown) {
-    return {
-      top: -nibHeight,
-      left: leftOffset + elementHalfWidth,
-      transform: 'rotate(0deg)'
-    }
+    transform = 'rotate(0deg)';
   } else if (direction.pointsLeft) {
-    return {
-      top: elementHalfHeight,
-      left: elementWidth - 2,
-      transform: 'rotate(90deg)'
-    }
+    transform = 'rotate(90deg)';
   } else if (direction.pointsUp) {
-    return {
-      top: elementHeight,
-      left: leftOffset + elementHalfWidth,
-      transform: 'rotate(180deg)'
-    }
+    transform = 'rotate(180deg)';
   } else {
-    return {
-      top: elementHalfHeight,
-      left: -nibWidth + 2,
-      transform: 'rotate(270deg)'
-    }
+    transform = 'rotate(270deg)';
+  }
+  if (left === null) {
+    top = 0;
+    left = 0;
+  }
+  return {
+    top: top,
+    left: left,
+    transform: transform
   }
 }
