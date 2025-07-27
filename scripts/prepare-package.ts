a171b1bb603f4ed09da8f17c14ae253cecb5be1d
Endri Gjiri <egjiri@gmail.com>
2025-07-26 21:41:06 -0400

    Remove resolveJsonModule from the tsconfig

--- scripts/prepare-package.ts
+++ scripts/prepare-package.ts
@@ -1,6 +1,6 @@
-import packageJSON from '../package.json';
 import { removeKeys } from '../src/objects';
-import { writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 
+const packageJSON = JSON.parse(readFileSync('package.json', 'utf8'));
 const newPackageJSON = removeKeys(packageJSON, 'scripts', 'devDependencies');
 writeFileSync('build/package.json', JSON.stringify(newPackageJSON, null, 2));
