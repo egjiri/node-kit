3ed97c309cc193c175bc4f6a114e2409f7c7d5b3
Endri Gjiri <egjiri@gmail.com>
2025-09-01 15:41:50 -0400

    Add tests for the getLastDayOfYear function

---
+++ src/time/get-last-day-of-year.test.ts
@@ -0,0 +1,16 @@
+import { getLastDayOfYear } from '.';
+import type { Cases } from 'testing';
+
+describe('getLastDayOfYear', () => {
+  const currentYear = new Date().getFullYear();
+
+  const cases: Cases<typeof getLastDayOfYear> = [
+    ['returns December 31 of current year when called with no parameter', [], new Date(currentYear, 11, 31)],
+    ['returns December 31 of current year when called with "this-year"', ['this-year'], new Date(currentYear, 11, 31)],
+    ['returns December 31 of next year when called with "next-year"', ['next-year'], new Date(currentYear + 1, 11, 31)],
+  ];
+  test.each(cases)('%s', (_, args, expected) => {
+    const actual = getLastDayOfYear(...args);
+    expect(actual).toEqual(expected);
+  });
+});
